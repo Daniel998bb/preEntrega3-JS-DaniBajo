695d651b6e9814657afffecf4d8fab6ebd91cc7d
Daniel998bb <daniel998b@gmail.com>
2024-04-19 21:00:24 +0200

    se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,124 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+
+let carrito = [];
+
+// Verificar si hay datos guardados en localStorage al cargar la página
+if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+}
+
+// Función para guardar el carrito en localStorage
+function guardarCarrito() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+productos.forEach((product) => {
+    let content = document.createElement("div");
+
+    content.className = "card";
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3> ${product.nombre}</h3>
+        <p class="prrice">${product.precio} $</p>
+    `;
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+            cantidad: 1
+        });
+        console.log("Producto agregado al carrito:", carrito);
+        guardarCarrito(); // Guardar el carrito en localStorage después de agregar un producto
+    });
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product, index) => {
+
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+            <img src="${product.img}">
+            <h3>${product.nombre}</h3>
+            <p>${product.precio} $</p>
+        `;
+
+        // Selector de cantidad de productos
+        let cantidadInput = document.createElement("input");
+        cantidadInput.type = "number";
+        cantidadInput.min = "1";
+        cantidadInput.value = product.cantidad;
+        cantidadInput.addEventListener("change", () => {
+            // Actualizar la cantidad de productos en el carrito
+            product.cantidad = parseInt(cantidadInput.value);
+            console.log("Cantidad actualizada:", carrito);
+            actualizarPrecioTotal();
+            guardarCarrito(); // Guardar el carrito en localStorage después de actualizar la cantidad
+        });
+
+        carritoContent.append(cantidadInput);
+
+        // Botón para eliminar productos del carrito
+        let eliminarButton = document.createElement("button");
+        eliminarButton.innerText = "Eliminar";
+        eliminarButton.className = "eliminar-button";
+
+        eliminarButton.addEventListener("click", () => {
+            // Eliminar el producto del carrito y actualizar el precio total
+            console.log("Antes de eliminar:", carrito);
+            carrito.splice(index, 1);
+            console.log("Después de eliminar:", carrito);
+            carritoContent.remove();
+            actualizarPrecioTotal();
+            guardarCarrito(); // Guardar el carrito en localStorage después de eliminar un producto
+        });
+
+        carritoContent.append(eliminarButton);
+        modalContainer.append(carritoContent);
+    });
+
+    // Calcular el precio total y mostrarlo
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+    const totalBuying = document.createElement("div");
+    totalBuying.className = "total-content";
+    totalBuying.innerText = `Total a pagar: ${total} $`;
+    modalContainer.append(totalBuying);
+});
+
+// Función para actualizar el precio total
+function actualizarPrecioTotal() {
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+    const totalBuying = document.querySelector(".total-content");
+    totalBuying.innerText = `Total a pagar: ${total} $`;
+}
---
+++ js/products.js
@@ -0,0 +1,26 @@
+const productos =[
+    {
+        id: 1,
+        nombre:"Reloj Rólex Datejust36",
+        precio: 9300,
+        img:"https://media.rolex.com/image/upload/q_auto/f_auto/t_v7/c_limit,w_1920/v1/catalogue/2024/upright-c/m126234-0051",
+    },
+    {
+        id: 2,
+        nombre:"Cazadora recta con cremallera. Hermès",
+        precio: 11000,
+        img:"https://assets.hermes.com/is/image/hermesproduct/cazadora-recta-con-cremallera--453100HB1A-worn-1-0-0-800-800_g.jpg",
+    },
+    {
+        id: 3,
+        nombre:"Bolso Twist MM. Louis Vuitton",
+        precio: 3900,
+        img:"https://es.louisvuitton.com/images/is/image/lv/1/PP_VP_L/louis-vuitton-bolso-twist-mm-piel-epi-bolsos--M24765_PM2_Front%20view.png?wid=1090&hei=1090",
+    },
+    {
+        id: 4,
+        nombre:"MINIVESTIDO DE MANGA CORTA MEDUSA '95. Versace",
+        precio: 1590,
+        img:"https://www.versace.com/dw/image/v2/BGWN_PRD/on/demandware.static/-/Sites-ver-master-catalog/default/dwb6d89240/original/90_1013214-1A10091_1PR20_10_Medusa95Short~SleevedMiniDress-Clothing-Versace-online-store_0_2.jpg?sw=850&q=85&strip=true",
+    },
+];
